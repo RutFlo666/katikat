2b8932ff74f4087914f818d30864a1242932d8a7
opilane <opilane@HP-CZC0359NN9>
2024-12-12 13:35:19 +0200

    Add project files.

---
+++ katikat/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace katikat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int bigNumber = 1244444445;
+            Console.WriteLine(bigNumber);
+
+            double comaNumber =12.5;
+            Console.WriteLine(comaNumber);
+
+            string stringVar = "asdasd12354@# $%&*()-+";
+            Console.WriteLine(stringVar);
+
+            float floatVarible = 123.123F;
+            Console.WriteLine(floatVarible);
+
+            char charVarible = '2';
+            Console.WriteLine(charVarible);
+
+            bool boolVarible = false;
+            Console.WriteLine(boolVarible);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
